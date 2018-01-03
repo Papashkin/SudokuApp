64aebf19dc6c788c596bb2ad782da7a734038701
Papashkin <fynjirby1>
2018-01-03 23:46:06 +0300

    Upd # 2 - home, 03.01.18 - update the screen.

--- src/SudokuApp/MainScreen.java
+++ src/SudokuApp/MainScreen.java
@@ -13,35 +13,44 @@ public class MainScreen extends JFrame {
     private JPanel panel2 = new JPanel();
     private JButton start = new JButton("Start");
     private JButton load = new JButton("Load");
-    private JTextField[] gameField = new JTextField[81];
-    private JButton back = new JButton("back");
+    private JButton back = new JButton("Back");
+    private JTextField gameField;
 
     MainScreen(){
         setTitle("Sudoku");
+        setBackground(Color.cyan);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400,300);
+        setSize(300,500);
+        start.setSize(100,50);
+        load.setSize(100,50);
+        start.setBorderPainted(false);
+        start.setBackground(Color.cyan);
 
-        panel1.setVisible(true);
-        panel1.setLayout(new GridLayout(2,1));
+        gameField = new JTextField ("", 9);
+        start.addActionListener(new StartEvent());
+        back.addActionListener(new BackEvent());
+
+//        panel1.setLayout(new BorderLayout());
+        panel1.setLayout(new FlowLayout(FlowLayout.LEADING,110,100));
         panel1.add(start);
         panel1.add(load);
-        panel1.setVisible(true);
 
-        panel1.setLayout(new GridLayout(10,9));
-//        for (int i = 1; i < gameField.length; i++){
-//            panel2.add(gameField[i]);
-//        }
+        panel2.setLayout(new GridLayout(10, 9));
+        panel2.add(gameField);
+        gameField.setSize(2,2);
         panel2.add(back);
-        panel2.setVisible(false);
 
-        setContentPane(panel1);
         setContentPane(panel2);
+        setContentPane(panel1);
+
     }
 
     class StartEvent implements ActionListener{
         public void actionPerformed(ActionEvent e){
             panel1.setVisible(false);
             panel2.setVisible(true);
+            setContentPane(panel2);
+            repaint();
         }
     }
 
@@ -54,6 +63,8 @@ public class MainScreen extends JFrame {
         public void actionPerformed(ActionEvent e){
             panel1.setVisible(true);
             panel2.setVisible(false);
+            setContentPane(panel1);
+            repaint();
         }
     }
 }
