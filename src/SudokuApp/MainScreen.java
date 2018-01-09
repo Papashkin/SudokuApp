63816147e154fd3396a3b1f4c65a20ad4abcae17
Papashkin <Papashkin-90@yandex.ru>
2018-01-09 15:46:52 +0300

    Upd #4 - work, 09.01.18 - new label and new table in the main Sudoku screen.

--- src/SudokuApp/MainScreen.java
+++ src/SudokuApp/MainScreen.java
@@ -3,6 +3,7 @@ package SudokuApp;
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.table.TableColumn;
+import javax.swing.table.TableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,19 +15,35 @@ public class MainScreen extends JFrame {
     private JButton start = new JButton("Start");
     private JButton load = new JButton("Load");
     private JButton back = new JButton("Back");
-    private JTable gameField = new JTable(9,9);
+    private JTable gameField = new JTable(9,9){
+        @Override
+        public boolean isCellEditable(int rowIndex, int columnIndex) {
+            return false;
+        }
+    };
+    private JLabel number_title = new JLabel("Числа для заполнения:");
+    private JTable number_row = new JTable(1,9){
+        @Override
+        public boolean isCellEditable(int rowIndex, int columnIndex) {
+            return false;
+        }
+    };
+    private int selected = 0;
+    private Font bigFont = new Font("Gothic", Font.BOLD, 24);
 
     MainScreen(){
         setTitle("Sudoku");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300,400);
+        setSize(300,430);
 
         start.setSize(100,50);
         start.setBackground(Color.white);
+        start.setFont(bigFont);
         start.addActionListener(new StartEvent());
         start.setBorderPainted(false);
 
         load.setSize(100,50);
+        load.setFont(bigFont);
         load.setBackground(Color.white);
         load.addActionListener(new LoadEvent());
         load.setBorderPainted(false);
@@ -39,29 +56,48 @@ public class MainScreen extends JFrame {
         gameField.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         for (int i = 0; i < gameField.getColumnCount();i++){
             TableColumn column = gameField.getColumnModel().getColumn(i);
-            column.setPreferredWidth(30);
+            column.setPreferredWidth(28);
             for (int j = 0; j < gameField.getRowCount();j++){
-                gameField.setRowHeight(j, 30);
+                gameField.setRowHeight(j, 28);
             }
         }
-//        gameField.setSize(270,360);
         gameField.setBackground(Color.white);
-
         Border compound, bevel1, bevel2;
         bevel1 = BorderFactory.createRaisedBevelBorder();
         bevel2 = BorderFactory.createLoweredBevelBorder();
         compound = BorderFactory.createCompoundBorder(bevel1, bevel2);
         gameField.setBorder(compound);
-//        gameField.addAncestorListener();
+        gameField.setColumnSelectionAllowed(true);
+        gameField.setRowSelectionAllowed(true);
 
-        panel1.setLayout(new FlowLayout(FlowLayout.CENTER,100,50));
+        number_row.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        number_row.setBackground(Color.white);
+        for (int i = 0; i < number_row.getColumnCount();i++){
+            number_row.isCellEditable(0, i);
+            number_row.setValueAt(i+1,0,i);
+            TableColumn a = number_row.getColumnModel().getColumn(i);
+            a.setPreferredWidth(28);
+        }
+        number_row.setRowHeight(28);
+        number_row.setColumnSelectionAllowed(true);
+        number_row.setRowSelectionAllowed(true);
+        number_row.enableInputMethods(true);
+
+        for (int j = 0; j < number_row.getColumnCount();j++){
+            if (number_row.isCellSelected(0,j)){
+                selected = (int)number_row.getValueAt(0,j);
+            }
+        }
+
+        panel1.setLayout(new FlowLayout(FlowLayout.CENTER,100,100));
         panel1.add(start);
         panel1.add(load);
         panel1.setBackground(Color.white);
 
-//        panel2.setLayout(new GridLayout(3,3));
-        panel2.setLayout(new FlowLayout(FlowLayout.CENTER,0,20));
+        panel2.setLayout(new FlowLayout(FlowLayout.CENTER,0,10));
         panel2.add(gameField);
+        panel2.add(number_title);
+        panel2.add(number_row);
         panel2.add(back);
         panel2.setBackground(Color.white);
 
