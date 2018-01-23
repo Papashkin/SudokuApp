960378683fd81baabaa22569f1fb5d22ab76756c
Papashkin <Papashkin-90@yandex.ru>
2018-01-23 17:57:33 +0300

    Upd #8 - work, 23.01.18 - fine cells filling. All prepare to sudoku algorithm realisation.

--- src/SudokuApp/MainScreen.java
+++ src/SudokuApp/MainScreen.java
@@ -1,27 +1,27 @@
 package SudokuApp;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableCellRenderer;
-import javax.swing.table.TableColumn;
+import javax.swing.table.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class MainScreen extends JFrame {
+public class MainScreen extends JFrame{
 
-    private JPanel panel1 = new JPanel();
-    private JPanel panel2 = new JPanel();
+    private JPanel mainPanel = new JPanel();
+    private JPanel gamePanel = new JPanel();
     private JButton start = new JButton("Start");
     private JButton load = new JButton("Load");
     private JButton back = new JButton("Back");
-    private JTable gameField = new JTable(9,9){
-//        @Override
-//        public boolean isCellEditable(int rowIndex, int columnIndex) {
-//            return false;
-//        }
+
+    private DefaultTableModel dtm = new DefaultTableModel(9,9);
+    private JTable gameField = new JTable(dtm){
+        @Override
+        public boolean isCellEditable(int rowIndex, int columnIndex) {
+            return false;
+        }
     };
+
     private JLabel number_title = new JLabel("Числа для заполнения:");
     private JTable number_row = new JTable(1,9){
         @Override
@@ -37,7 +37,7 @@ public class MainScreen extends JFrame {
     MainScreen(){
         setTitle("Sudoku");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300,440);
+        setSize(300,445);
         setLocation(200,100);
         setResizable(false);
 
@@ -51,44 +51,25 @@ public class MainScreen extends JFrame {
         back.addActionListener(new BackEvent());
 
         setParameters(gameField, numberFont);
-        for (int i = 0; i < gameField.getColumnCount();i++){
-            TableColumn column = gameField.getColumnModel().getColumn(i);
-            column.setPreferredWidth(30);
-            for (int j = 0; j < gameField.getRowCount();j++){
-//                column.setCellRenderer(new JustCellRenderer(i,j));
-            }
-        }
-        setTheField(0);
-        gameField.setFillsViewportHeight(true);
-
         setParameters(number_row, numberFont);
-        number_row.enableInputMethods(true);
         for (int i = 0; i < number_row.getColumnCount();i++){
-            TableColumn a = number_row.getColumnModel().getColumn(i);
-            a.setPreferredWidth(30);
             number_row.isCellEditable(0, i);
             number_row.setValueAt(i+1,0,i);
         }
-        for (int j = 0; j < number_row.getColumnCount();j++){
-            if (number_row.isCellSelected(0,j)){
-                selected = (int)number_row.getValueAt(0,j);
-            }
-        }
 
-        panel1.setLayout(new FlowLayout(FlowLayout.CENTER,100,110));
-        panel1.add(start);
-        panel1.add(load);
-        panel1.setBackground(Color.white);
+        mainPanel.setLayout(new FlowLayout(FlowLayout.CENTER,100,110));
+        mainPanel.add(start);
+        mainPanel.add(load);
+        mainPanel.setBackground(Color.white);
 
-        panel2.setLayout(new FlowLayout(FlowLayout.CENTER,0,15));
-        panel2.add(gameField);
-        panel2.add(number_title);
-        panel2.add(number_row);
-        panel2.add(back);
-        panel2.setBackground(Color.white);
+        gamePanel.setLayout(new FlowLayout(FlowLayout.CENTER,0,15));
+        gamePanel.add(gameField);
+        gamePanel.add(number_title);
+        gamePanel.add(number_row);
+        gamePanel.add(back);
+        gamePanel.setBackground(Color.white);
 
-        setContentPane(panel2);
-        setContentPane(panel1);
+        setContentPane(mainPanel);
     }
 
     class StartEvent extends Component implements ActionListener{
@@ -100,11 +81,9 @@ public class MainScreen extends JFrame {
                     "Levels",JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,
                     null,level,10);
             if (lvl == 0 || lvl == 1 || lvl == 2) {
-                setTheField(lvl);
-                panel1.setVisible(false);
-                panel2.setVisible(true);
-                panel2.repaint();
-                setContentPane(panel2);
+                setTheField(gameField, lvl);
+                setContentPane(gamePanel);
+                gamePanel.repaint();
             }
         }
     }
@@ -116,20 +95,18 @@ public class MainScreen extends JFrame {
 
     class BackEvent implements ActionListener{
         public void actionPerformed(ActionEvent e){
-            panel1.setVisible(true);
-            panel2.setVisible(false);
-            setContentPane(panel1);
+            setContentPane(mainPanel);
             number_row.clearSelection();
-            repaint();
+            gameField.clearSelection();
         }
     }
 
-    private void setTheField(int lvl){
-        int columns = gameField.getColumnCount();
-        int rows = gameField.getRowCount();
+    private void setTheField(JTable table, int lvl){
+        int columns = table.getColumnCount();
+        int rows = table.getRowCount();
         int[] aRow = {1,2,3,4,5,6,7,8,9};
         for (int j = 0; j < columns; j++){
-            gameField.setValueAt(aRow[j],0,j);
+            table.setValueAt(aRow[j],0,j);
         }
         for (int i = 1; i < rows; i++){
             if (i == 3 || i == 6){
@@ -137,13 +114,14 @@ public class MainScreen extends JFrame {
             }
             aRow = shiftLeft(aRow, 3);
             for (int j = 0; j < columns; j++){
-                gameField.setValueAt(aRow[j],i,j);
+                table.setValueAt(aRow[j],i,j);
             }
         }
+        dtm.fireTableDataChanged();
+        table.repaint();
     }
 
     public int[] shiftLeft (int[] vector, int shiftCount){
-        int index = shiftCount - 1;
         int[] vectorEnd = new int[vector.length];
         for (int j = 0 ;j < shiftCount;j++){
             vectorEnd[j+(vector.length)-shiftCount] = vector[j];
@@ -154,11 +132,21 @@ public class MainScreen extends JFrame {
         return vectorEnd;
     }
 
-    private static void setParameters(JTable table, Font aFont){
+    private void setParameters(JTable table, Font aFont){
         table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         table.setBackground(Color.white);
         table.setBorder(BorderFactory.createMatteBorder(2,2,2,2, Color.black));
+        gameField.enableInputMethods(true);
         table.setRowHeight(30);
+        for (int i = 0; i < table.getColumnCount();i++) {
+            TableColumn tabCol = table.getColumnModel().getColumn(i);
+            tabCol.setPreferredWidth(30);
+            if (table.getRowCount() != 1){
+                for (int j = 0; j < table.getRowCount(); j++) {
+                    tabCol.setCellRenderer(new SudokuCellRenderer(i,j));
+                }
+            }
+        }
         table.setColumnSelectionAllowed(true);
         table.setRowSelectionAllowed(true);
         table.setFont(aFont);
@@ -166,36 +154,10 @@ public class MainScreen extends JFrame {
         cellRenderer.setHorizontalAlignment(SwingConstants.CENTER);
     }
 
-    private static void setBtnParameters(JButton button, Font aFont){
+    private void setBtnParameters(JButton button, Font aFont){
         button.setSize(100,50);
         button.setBackground(Color.white);
         button.setFont(aFont);
         button.setBorderPainted(false);
-    }
-
-    public class JustCellRenderer extends JLabel implements TableCellRenderer{
-        @Override
-        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
-                                                       boolean hasFocus, int row, int column) {
-            if (column == 2 || column == 5){
-                if (row == 2 || row == 5){
-                    setBorder(BorderFactory.createMatteBorder(0, 0, 2, 2, Color.black));
-                } else {
-                    setBorder(BorderFactory.createMatteBorder(0, 0, 0, 2, Color.black));
-                }
-            }
-            else {
-                if (row == 2 || row == 5){
-                    setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, Color.black));
-                } else {
-                    setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.black));
-                }
-            }
-            setHorizontalAlignment(SwingConstants.CENTER);
-            return this;
-        }
-
-        public JustCellRenderer(int columnIndex, int rowIndex){
-        }
     }
 }
---
+++ src/SudokuApp/SudokuCellRenderer.java
@@ -0,0 +1,54 @@
+package SudokuApp;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import java.awt.*;
+
+public class SudokuCellRenderer extends DefaultTableCellRenderer {
+    Font font = new Font("Arial", Font.BOLD, 18);
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
+                                                   boolean hasFocus, int row, int column) {
+            if (column == 2 || column == 5){
+                if (row == 2 || row == 5){
+                    setBorder(BorderFactory.createMatteBorder(0, 0, 2, 2, Color.black));
+                } else {
+                    setBorder(BorderFactory.createMatteBorder(0, 0, 0, 2, Color.black));
+                }
+            }
+            else {
+                if (row == 2 || row == 5){
+                    setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, Color.black));
+                } else {
+                    setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.black));
+                }
+            }
+            setHorizontalAlignment(SwingConstants.CENTER);
+        setVisible(true);
+        setValue(value);
+        setFont(font);
+        return this;
+    }
+
+    public SudokuCellRenderer(int column, int row){
+//        if (column == 2 || column == 5){
+//            if (row == 2 || row == 5){
+//                setBorder(BorderFactory.createMatteBorder(0, 0, 2, 2, Color.black));
+//            } else {
+//                setBorder(BorderFactory.createMatteBorder(0, 0, 0, 2, Color.black));
+//            }
+//        }
+//        else {
+//            if (row == 2 || row == 5){
+//                setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, Color.black));
+//            } else {
+//                setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.black));
+//            }
+//        }
+//        setHorizontalAlignment(SwingConstants.CENTER);
+//        setOpaque(true);
+//        setValue(0);
+//        repaint();
+    }
+}
